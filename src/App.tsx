fd9e94504f68ae6781880ef3ce40b84a759e6791
Alena Shapovalova <avshapovalova31@gmail.com>
2023-06-30 00:05:53 +0200

    feat: add menu for small and large device

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from './components/MainPage/MainPage';
 import Card from './components/Card/Card';
+import Navbar from './components/Navbar/Navbar';
+
 
 
 function App() {
   return (
     <div className="App">
-      <MainPage />
+       <BrowserRouter>
+        <Navbar />
+        <Routes >
+          <Route path='/' element={<MainPage />} />
+          <Route path='/card' element={<Card />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,31 +1,40 @@
+import { useState } from "react";
 import "./Navbar.css";
 
-const Navbar= () =>{
+const Navbar = () => {
+
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
     <nav className="nav container">
-        <a href="#" className="nav__logo">
-            XCOMPANY
-        </a>
-        <div className="nav__menu" id="nav-menu">
-            <ul className="nav__list">
-                <li className="nav__item">
-                    <a href="#" className="nav__link">Home</a>
-                </li>
-                <li className="nav__item">
-                    <a href="#" className="nav__link">About</a>
-                </li>
-                <li className="nav__item">
-                    <a href="#" className="nav__link">Contact</a>
-                </li>
-            </ul>
+      <a href="/" className="nav__logo">
+        MYLINKS
+      </a>
+      <div className="nav__menu" id="nav-menu">
+        <ul className="nav__list">
+          <li className="nav__item">
+            <a href="/card" className="nav__link">Card</a>
+          </li>
+          <li className="nav__item">
+            <a href="#" className="nav__link">About</a>
+          </li>
+          <li className="nav__item">
+            <a href="#" className="nav__link">Contact</a>
+          </li>
+        </ul>
 
-            <div className="nav__close" id="nav-close">
-                <i className='bx bx-x'></i>
-            </div>
+        <div className="nav__close" id="nav-close">
+        <i className="ri-close-line"></i>
         </div>
+      </div>
 
-        <div className="nav__toggle" id="nav-toggle">
-            <i className='bx bx-grid-alt'></i>
-        </div>
+      <div className="nav__toggle" id="nav-toggle">
+      <i className="ri-menu-line"></i>
+      </div>
     </nav>
-}
+  )
+};
+
 export default Navbar;
