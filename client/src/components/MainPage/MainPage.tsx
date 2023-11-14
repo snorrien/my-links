c7cdf3762a011b2d74c3de8ab5dc9a5cffe7b086
Alena Shapovalova <avshapovalova31@gmail.com>
2023-11-14 17:42:39 +0100

    fix styles

--- client/src/components/MainPage/MainPage.tsx
+++ client/src/components/MainPage/MainPage.tsx
@@ -24,24 +24,6 @@ const MainPage = () => {
             </section>
             <section className="about">
                 <div className="about__container">
-                    <div className="about__name">
-                        <h1 className="about__title">About platform</h1>
-                        <p className="about__subtitle">MyLink is the ultimate online platform for effortlessly saving and organizing all
-                            your favorite web links in one convenient place. </p>
-                    </div>
-                    <div className="about__description">
-                        <div className="description-element">
-                            <p className="description-title">Save</p>
-                            <p>Say goodbye to cluttered browser
-                                bookmarks and scattered links across multiple devices. No more worrying about losing your links when switching devices.</p>
-                        </div>
-                        <div className="description-element">
-                            <p className="description-title"> Organize</p>
-                            <p>Organize and categorize web links with just a few clicks.
-                                Intuitive interface for quick and easy link entry and retrieval.
-                                Assign tags and descriptions to links for enhanced searchability.</p>
-                        </div>
-                    </div>
                     <div className="about__list">
                         <div className="about__list-scroll">
                             <div className="about__list-li">
@@ -60,25 +42,45 @@ const MainPage = () => {
                             </div>
                         </div>
                     </div>
-                    <div className="about__way">
-
+                    <div className="about__name">
+                        <h1 className="about__title">About platform</h1>
+                        <p className="about__subtitle">MyLink is the ultimate online platform for effortlessly saving and organizing all
+                            your favorite web links in one convenient place. </p>
                     </div>
-                    <div className="about__me">
-                        <p>Hi!</p>
-                        <p>In this site I tried to implement the functionality that I personally needed.
-                            If there isn't something that satisfies my needs, I'll just do it.
-                            That's how this project came about.</p>
-                        <div className="me__author">
-                            <img className="author-image" src="imgs/author.jpg" />
-                            <div className="author-data">
-                                <p className="author-name">Alena Shapovalova</p>
-                                <p className="author-profession">Frontend Developer</p>
+                    <div className="about__description">
+                        <div className="description-element">
+                            <p className="description-title">Save</p>
+                            <p>Say goodbye to cluttered browser
+                                bookmarks and scattered links across multiple devices. No more worrying about losing your links when switching devices.</p>
+                        </div>
+                        <div className="description-element">
+                            <p className="description-title"> Organize</p>
+                            <p>Organize and categorize web links with just a few clicks.
+                                Intuitive interface for quick and easy link entry and retrieval.
+                                Assign tags and descriptions to links for enhanced searchability.</p>
+                        </div>
+                        <div className="description__me">
+                            <p>Hi!</p>
+                            <p>In this site I tried to implement the functionality that I personally needed.
+                                If there isn't something that satisfies my needs, I'll just do it.
+                                That's how this project came about.</p>
+                            <div className="me__author">
+                                <img className="author-image" src="imgs/author.jpg" />
+                                <div className="author-data">
+                                    <p className="author-name">Alena Shapovalova</p>
+                                    <p className="author-profession">Frontend Developer</p>
+                                </div>
                             </div>
                         </div>
-
                     </div>
+
                 </div>
+
             </section >
+            <section className="start">
+                <h1 className="start__title">Ready to try MyLinks?</h1>
+                <button className="button-start">Start</button>
+            </section>
 
         </div >
     );
